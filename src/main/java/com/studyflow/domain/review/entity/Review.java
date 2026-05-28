15b89373266b91537a83764d4ca0a33b4e948bdc
gusdnzla26-art <gusdnzla26@gmail.com>
2026-05-28 16:04:49 +0900

    feat: add review entity

---
+++ src/main/java/com/studyflow/domain/review/entity/Review.java
@@ -0,0 +1,22 @@
+package com.studyflow.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+}
