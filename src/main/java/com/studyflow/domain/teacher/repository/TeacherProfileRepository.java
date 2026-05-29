a0a513ef6921212f397a1d721ae3740098ab01fb
DESKTOP-QNAFNH9\leejaeseop <vavonae@gmail.com>
2026-05-29 17:47:53 +0900

    feat : findbyid 추가

--- src/main/java/com/studyflow/domain/teacher/repository/TeacherProfileRepository.java
+++ src/main/java/com/studyflow/domain/teacher/repository/TeacherProfileRepository.java
@@ -1,7 +1,12 @@
 package com.studyflow.domain.teacher.repository;
 
 import com.studyflow.domain.teacher.entity.TeacherProfile;
+import com.studyflow.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeacherProfileRepository extends JpaRepository<TeacherProfile, Long> {
+    Optional<TeacherProfile> findById(Long id);
+
 }
